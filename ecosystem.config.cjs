af253fc92bfd71739ebb18cc3a3520b641bcfbf7
liaoqinwei <2573424017@qq.com>
2024-03-11 19:44:14 +0800

    feat:deploy config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,7 +4,6 @@ module.exports= {
     script: './.output/server/index.mjs',
     env_production: {
       NODE_ENV: "production",
-      DATABASE_URL: "postgresql://postgres:123@localhost:5432/test?schema=public"
     },
     env_development: {
       NODE_ENV: "development"
